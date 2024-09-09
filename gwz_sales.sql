a67adb267f005ed76bb5c0e4864312f8902de42e
Adshayaselva <adshaya.selva02@gmail.com>
2024-09-09 15:53:29 +0100

    rounding turnover and pc

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,6 @@
 select date_date,
-sum(turnover) as turnover 
-sum(purchase_cost) as purchase_cost 
+round(sum(turnover),2) as turnover 
+round(sum(purchase_cost),2) as purchase_cost 
 from moonlit-web-433710-n7.course16_1.gwz_sales
 group by date_date
 order by date_date
