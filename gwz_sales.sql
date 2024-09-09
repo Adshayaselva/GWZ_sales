a3ec43b6e9787dcb46919d2d9fff734f7ab924a2
Adshayaselva <adshaya.selva02@gmail.com>
2024-09-09 14:26:44 +0100

    purchase cost

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,2 +1,6 @@
-select turnover
+select date_date,
+sum(turnover) as turnover 
+sum(purchase_cost) as purchase_cost 
 from moonlit-web-433710-n7.course16_1.gwz_sales
+group by date_date
+order by date_date
